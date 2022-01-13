9f8476155629990cbef29f4720692e9b8c9a1640
gusygusy <92151773+gusygusy@users.noreply.github.com>
2022-01-13 17:14:39 +0300

    Update cred.py

--- t_bot/cred.py
+++ t_bot/cred.py
@@ -1,3 +1,3 @@
-bot_token = "5075257732:AAG84yGq1sEElKt7jOc58WUXih8Mdf4Zqr4"
-bot_user_name = "pizza_qwint_bot"
-URL = "https://congadance.pythonanywhere.com/"
+bot_token = ""
+bot_user_name = ""
+URL = ""
